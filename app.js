6bfe56d4d30b9853232e4ed62f5b86ee9a156427
MX <242296165@qq.com>
2016-03-13 22:33:11 +0800

    add log

--- app.js
+++ app.js
@@ -11,12 +11,20 @@ var MongoStore = require('connect-mongo')(session);//保存session到mongodb
 var favicon = require('serve-favicon');
 var routes = require('./routes/index');
 var mongoose = require('mongoose');
-var fs = require('fs');
-var app = express();
 var settings = require('./settings');
 var flash = require('connect-flash');
 var moment = require('moment');
 var markdown = require('markdown').markdown;
+var fs = require('fs');
+var app = express();
+var FileStreamRotator = require('file-stream-rotator');
+
+var logDirectory = __dirname+'/logs';
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);//保证日志目录存在
+
+
+
+
 //连接数据库
 var dbUrl = 'mongodb://localhost:27017/blog';
 mongoose.connect(dbUrl);
@@ -58,13 +66,28 @@ app.use(session({
     })
 }));
 app.use(favicon(path.join(__dirname,'public/favicon.ico')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 routes(app);
+
+if('development' === app.get('env')){
+    app.set('showStackError',true);
+    app.use(logger('dev'));
+    app.locals.pretty = true;
+}else{
+    var accessLogStream = FileStreamRotator.getStream({
+        date_format:'YYYYMMDD',
+        filename:logDirectory+'/access-%DATE%.log',
+        frequency:'daily',
+        verbose:false
+    });
+    app.use(logger('combined',{stream: accessLogStream}));
+}
+
 moment.locale('zh-cn');
 app.locals.moment = moment;
 app.locals.markdown = markdown;
